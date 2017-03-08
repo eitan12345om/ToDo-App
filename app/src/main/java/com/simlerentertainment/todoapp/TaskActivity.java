b4b25c43310fda17faf7d85e6ec31176e6aede5b
Eitan Simler <eitan.simler@gmail.com>
2017-03-07 23:15:39 -0800

    Completed initial version of app

--- app/src/main/java/com/simlerentertainment/todoapp/TaskActivity.java
+++ app/src/main/java/com/simlerentertainment/todoapp/TaskActivity.java
@@ -1,17 +1,82 @@
 package com.simlerentertainment.todoapp;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.simlerentertainment.todoapp.db.TaskContract;
+import com.simlerentertainment.todoapp.db.TaskDbHelper;
+
+import java.util.ArrayList;
+
 public class TaskActivity extends AppCompatActivity {
+
+    private static final String TAG = "TaskActivity";
+    private TaskDbHelper mHelper;
+    private ListView mTaskListView;
+    private ArrayAdapter<String> mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        mHelper = new TaskDbHelper(this);
+        mTaskListView = (ListView) findViewById(R.id.list_todo);
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        ArrayList<String> taskList = new ArrayList<>();
+        SQLiteDatabase sqLiteDatabase = mHelper.getReadableDatabase();
+        Cursor cursor = sqLiteDatabase.query(TaskContract.TaskEntry.TABLE,
+                new String[]{TaskContract.TaskEntry._ID, TaskContract.TaskEntry.COL_TASK_TITLE},
+                null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            int index = cursor.getColumnIndex(TaskContract.TaskEntry.COL_TASK_TITLE);
+            taskList.add(cursor.getString(index));
+        }
+
+        if (mAdapter == null) {
+            mAdapter = new ArrayAdapter<>(this,
+                    R.layout.task, // What view to use for the items
+                    R.id.task_title, // Where to put the string of data
+                    taskList); // Where to fo get the data
+            mTaskListView.setAdapter(mAdapter);
+        } else {
+            mAdapter.clear();
+            mAdapter.addAll(taskList);
+            mAdapter.notifyDataSetChanged();
+        }
+
+        cursor.close();
+        sqLiteDatabase.close();
+    }
+
+    public void deleteTask(View view) {
+        View parent = (View) view.getParent();
+        TextView taskTextView = (TextView) parent.findViewById(R.id.task_title);
+        String task = taskTextView.getText().toString();
+        SQLiteDatabase sqLiteDatabase = mHelper.getWritableDatabase();
+        sqLiteDatabase.delete(TaskContract.TaskEntry.TABLE,
+                TaskContract.TaskEntry.COL_TASK_TITLE + " = ?",
+                new String[]{task});
+        sqLiteDatabase.close();
+        updateUI();
     }
 
     @Override
@@ -23,13 +88,35 @@ public class TaskActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            // TODO: Add actual logic to add new task
             case R.id.add_task:
-                Toast.makeText(getApplicationContext(), "ADD", Toast.LENGTH_SHORT).show();
+                final EditText taskEditText = new EditText(this);
+                AlertDialog alertDialog = new AlertDialog.Builder(this)
+                        .setTitle("Add a new task")
+                        .setMessage("What do you want to do next?")
+                        .setView(taskEditText)
+                        .setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String task = taskEditText.getText().toString();
+                                SQLiteDatabase sqLiteDatabase = mHelper.getWritableDatabase();
+                                ContentValues contentValues = new ContentValues();
+                                contentValues.put(TaskContract.TaskEntry.COL_TASK_TITLE, task);
+                                sqLiteDatabase.insertWithOnConflict(TaskContract.TaskEntry.TABLE,
+                                        null,
+                                        contentValues,
+                                        SQLiteDatabase.CONFLICT_REPLACE);
+                                sqLiteDatabase.close();
+
+                                updateUI();
+                            }
+                        })
+                        .setNegativeButton("Cancel", null)
+                        .create();
+                alertDialog.show();
                 return true;
             // TODO: Add actual logic to switch to settings
             case R.id.settings:
-                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                Log.d(TAG, "Settings");
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
---
+++ app/src/main/java/com/simlerentertainment/todoapp/db/TaskContract.java
@@ -0,0 +1,17 @@
+package com.simlerentertainment.todoapp.db;
+
+import android.provider.BaseColumns;
+
+/**
+ * Created by Eitan on 3/7/2017.
+ */
+
+public class TaskContract {
+    public static final String DB_NAME = "com.simlerentertainment.todoapp.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+        public static final String TABLE = "tasks";
+        public static final String COL_TASK_TITLE = "title";
+    }
+}
