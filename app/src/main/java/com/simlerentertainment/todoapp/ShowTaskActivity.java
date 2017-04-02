4c1735dfca305e72016e531b0355ea168ab4daa5
Eitan Simler <eitan.simler@gmail.com>
2017-04-01 21:02:27 -0700

    Added cancel button

--- app/src/main/java/com/simlerentertainment/todoapp/ShowTaskActivity.java
+++ app/src/main/java/com/simlerentertainment/todoapp/ShowTaskActivity.java
@@ -10,6 +10,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
